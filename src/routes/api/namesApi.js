ac5e8c3fa91dd4d2eb2a0d3876e3cfdbe8cd23eb
Tudor Tocan <tudor.tocan@gmail.com>
2022-08-20 11:24:31 +0100

    add endpoints to add new name, get all names from db.json

--- src/routes/api/namesApi.js
+++ src/routes/api/namesApi.js
@@ -1,12 +1,36 @@
 const { Router } = require("express");
+const { v4 } = require("uuid");
+
+const getFromDB = require("../../utils/getFromDB");
+const writeToDB = require("../../utils/writeToDB");
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Hello James" });
+  // get data from the db.json
+  const names = getFromDB();
+
+  res.json(names);
 });
 
-router.post("/", () => {});
+router.get("/:id", (req, res) => {});
+
+router.post("/", (req, res) => {
+  const { body } = req;
+  const id = v4();
+  const names = getFromDB();
+
+  const newName = {
+    ...body,
+    id,
+  };
+
+  names.push(newName);
+
+  writeToDB(JSON.stringify(names));
+
+  res.json({ message: "Success" });
+});
 
 router.put("/:id", () => {});
 
